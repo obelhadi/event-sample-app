4ce30c20b2e7c854239f2acba98e10de449360e2
ouissambel <wessam.bel@gmail.com>
2018-03-04 23:11:45 +0100

    Initial commit

---
+++ src/main/java/org/obel/EventSampleAppApplication.java
@@ -0,0 +1,12 @@
+package org.obel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventSampleAppApplication.class, args);
+	}
+}
