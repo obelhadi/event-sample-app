7d5fa8d47906bd83e894b92b1b4fbc3e6d44a1db
ouissambel <wessam.bel@gmail.com>
2018-03-06 12:36:46 +0100

    Update elasticsearch config to use file properties vars

--- src/main/java/org/obel/config/ElasticSearchConfig.java
+++ src/main/java/org/obel/config/ElasticSearchConfig.java
@@ -5,6 +5,7 @@ import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,9 +17,10 @@ public class ElasticSearchConfig {
 
 
     @Bean
-    public Client esClient() throws UnknownHostException {
+    public Client esClient(@Value("${elasticsearch.host}") String esHost,
+                           @Value("${elasticsearch.port}") int port) throws UnknownHostException {
         return new PreBuiltTransportClient(Settings.EMPTY)
-                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(esHost), port));
     }
 
 }
